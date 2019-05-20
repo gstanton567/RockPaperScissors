41c3de4aef30433d8aa3859192a1f8690de023ab
Grayson Stanton <gstanton@unomaha.edu>
2019-05-20 13:27:03 -0500

    original

--- Rock Paper Scissors/ViewController.swift
+++ Rock Paper Scissors/ViewController.swift
@@ -10,11 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    var random = Int(CGFloat.random(in: (0...3)))
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onRockTap(_ sender: UITapGestureRecognizer) {
+        random = Int(CGFloat.random(in: (0...3)))
+        if random == 0
+        {
+            titleLabel.text = "Tie"
+        }
+        if random == 1
+        {
+            titleLabel.text = "You Lose!"
+        }
+        if random == 2
+        {
+            titleLabel.text = "You Win!"
+        }
+        print(random)
+    }
+    
+    @IBAction func onPaperTap(_ sender: UITapGestureRecognizer) {
+        random = Int(CGFloat.random(in: (0...3)))
+        if random == 0
+        {
+            titleLabel.text = "You Win!"
+        }
+        if random == 1
+        {
+            titleLabel.text = "Tie"
+        }
+        if random == 2
+        {
+            titleLabel.text = "You Lose!"
+        }
+        print(random)
+    }
+    @IBAction func onScissorTap(_ sender: UITapGestureRecognizer) {
+        
+        random = Int(CGFloat.random(in: (0...3)))
+        if random == 0
+        {
+            titleLabel.text = "You Lose!"
+        }
+        if random == 1
+        {
+            titleLabel.text = "You Win!"
+            
+        }
+        if random == 2
+        {
+            titleLabel.text = "Tie"
+        }
+        print(random)
+    }
 }
 
