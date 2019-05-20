3e79f309961ab20a3370761ff5e8ddfbf8b46dcf
Grayson Stanton <gstanton@unomaha.edu>
2019-05-20 15:54:54 -0500

    add pickerController Delegate

--- Rock Paper Scissors/ViewController.swift
+++ Rock Paper Scissors/ViewController.swift
@@ -12,13 +12,16 @@ import SafariServices
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
  {
     
+    @IBOutlet weak var scissorsImageView: UIImageView!
+    @IBOutlet weak var paperImageView: UIImageView!
+    @IBOutlet weak var rockImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var startButton: UIButton!
     
     var timer : Timer? = nil
     var i = 4
     var random = Int(CGFloat.random(in: (0...3)))
-    
+    var bool = 0
     override func viewDidLoad() {
         super.viewDidLoad()
             let tap = UITapGestureRecognizer(target: self, action: #selector(doubleTapped))
@@ -27,6 +30,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
 
     @IBAction func onRockTap(_ sender: UITapGestureRecognizer) {
+        bool = 1
         random = Int(CGFloat.random(in: (0...3)))
         if random == 0
         {
@@ -59,6 +63,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     @IBAction func onPaperTap(_ sender: UITapGestureRecognizer) {
+        bool = 2
         random = Int(CGFloat.random(in: (0...3)))
         if random == 0
         {
@@ -90,7 +95,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         print(random)
     }
     @IBAction func onScissorTap(_ sender: UITapGestureRecognizer) {
-        
+        bool = 3
         random = Int(CGFloat.random(in: (0...3)))
         if random == 0
         {
@@ -100,6 +105,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }
             startButton.isEnabled = true
             i = 4
+            
         }
         if random == 1
         {
@@ -119,6 +125,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }
             startButton.isEnabled = true
             i = 4
+            
         }
         print(random)
     }
@@ -159,7 +166,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @objc func doubleTapped() {
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
             let myPickerController = UIImagePickerController()
-            myPickerController.delegate = self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate
+            myPickerController.delegate = self as UIImagePickerControllerDelegate & UINavigationControllerDelegate
             myPickerController.sourceType = .photoLibrary
             self.present(myPickerController, animated: true, completion: nil)
         }
@@ -172,26 +179,51 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 //            }
 //
 //    }
-    func openCamera(){
-        if UIImagePickerController.isSourceTypeAvailable(.camera){
-            let myPickerController = UIImagePickerController()
-            myPickerController.delegate = self
-            myPickerController.sourceType = .camera
-            self.present(myPickerController, animated: true, completion: nil)
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+//            self.imagePickedBlock?(image)
+            if bool == 1
+            {
+            rockImageView.image = image
+            }
+            else if bool == 2
+            {
+                paperImageView.image = image
+            }
+            else if bool == 3
+            {
+                scissorsImageView.image = image
+            }
         }
     }
     
+//    func openCamera(){
+//
+//            let imagePicker =  UIImagePickerController()
+//            imagePicker.delegate = self
+//            switch source {
+//            case .camera:
+//                imagePicker.sourceType = .camera
+//            case .photoLibrary:
+//                imagePicker.sourceType = .photoLibrary
+//            }
+//            present(imagePicker, animated: true, completion: nil)
+//
+//    }
+    
     
     @IBAction func onRockLongPress(_ sender: UILongPressGestureRecognizer) {
-        openCamera()
+       
+//        openCamera()
     }
     
     @IBAction func onPaperLongPress(_ sender: UILongPressGestureRecognizer) {
-        openCamera()
+//        openCamera()
     }
     
     @IBAction func onScissorsLongPressed(_ sender: UILongPressGestureRecognizer) {
-        openCamera()
+//        openCamera()
     }
     
 }
