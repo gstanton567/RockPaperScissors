aa693e1520f0fbff0bcecdb5e0096dd845ad2545
Grayson Stanton <gstanton@unomaha.edu>
2019-05-20 12:24:52 -0500

    Initial Commit

---
+++ Rock Paper Scissors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rock Paper Scissors
+//
+//  Created by Grayson Stanton on 5/20/19.
+//  Copyright © 2019 Grayson Stanton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
